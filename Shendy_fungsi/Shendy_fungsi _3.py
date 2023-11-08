128c6da5160aed8a71293a0d627b95a47bb41464
ShendyTKJ <148422331+ShendyTKJ@users.noreply.github.com>
2023-11-08 17:21:51 +0700

    Add files via upload

---
+++ Shendy_fungsi/Shendy_fungsi _3.py
@@ -0,0 +1,16 @@
+# Nama : Shendy pratama n
+# Kls : XI TKJ 2
+# No : 27
+# Soal : Deskripsi Pekerjaan: Buatlah sebuah fungsi yang menghitung nilai pangkat dari suatu bilangan
+#        dengan eksponen tertentu.
+#        Rumus: Bilangan^Eksponen
+
+
+
+def pangkat_dari_bilangan(bilangan, eksponen):
+    return bilangan ** eksponen
+
+input_bilangan = int(input("Masukkan Bilangan: "))
+eksponen_bilangan = int(input("Masukkan Eksponen: "))
+
+print(f"{input_bilangan} pangkat {eksponen_bilangan} : {pangkat_dari_bilangan(input_bilangan, eksponen_bilangan)}")
---
+++ Shendy_perulangan/Shendy_perulangan_5.py
@@ -0,0 +1,16 @@
+# Nama : Shendy pratama n
+# Kls : XI TKJ 2
+# No : 27
+# Soal : Deskripsi Pekerjaan: Sebuah bakteri pembelahan setiap 20 menit. Sebuah bakteri ditempatkan
+#        dalam lingkungan yang cocok dan berkembang biak dengan cepat. Buatlah program yang
+#        menghitung berapa kali pembelahan bakteri terjadi dalam rentang waktu 2 jam.
+#        Rumus: Jumlah pembelahan setelah t menit = t / 20
+
+
+
+total_Waktu = 2 * 60
+waktu_pembelahan = 20
+
+total_pembelahan = total_Waktu // waktu_pembelahan
+
+print(f"Total pembelahan bakteri dalam rentang waktu 2 jam sebanyak : {total_pembelahan} kali")
